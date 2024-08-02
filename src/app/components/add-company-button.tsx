8dfe40097ff3640e0868ac3c6654697d113dd2e7
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-08-02 15:17:45 +0300

    add components

---
+++ src/app/components/add-company-button.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React, {useState} from 'react';
+import Button from "@/app/components/button";
+import dynamic from "next/dynamic";
+
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+  ssr: false,
+});
+
+function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal onSubmit={console.log} show={show} onClose={() => setShow(false)}/>
+    </>
+  );
+}
+
+export default AddCompanyButton;
