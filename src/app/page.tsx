53c7528d077ed0372bbc1288d63deee163bcdc82
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-08-02 13:02:23 +0300

    tailwind styles for status label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import StatusLabel, { Status } from "@/app/components/status-label";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-      <p className="text-4xl font-bold text-green-500">ghrh</p>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
