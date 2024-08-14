9d40c96f97083f16fbcf42e90c43766feb625c7e
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-08-14 19:01:38 +0300

    parallel routes

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import Header from '@/app/components/header';
 
 export interface PageProps {}
 
-function Page({}: PageProps) {
+export default function Page({}: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Dashboard page</h1>
-    </main>
+    <>
+      <Header>Dashboard</Header>
+    </>
   );
 }
-
-export default Page;
